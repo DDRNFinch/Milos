8d0b6876179ce675ddd7b7a0d9c3ccbda4dce775
DDRNFinch <dfinch1984@hotmail.co.uk>
2026-08-21 19:00:24 +0100

    Cache observation ZIP exporter for offline use

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "milos-assessor-shell-v2.2";
+const CACHE_NAME = "milos-assessor-shell-v2.3";
 const CACHE_PREFIX = "milos-assessor-shell-";
 const APP_SHELL = [
   "./",
@@ -18,6 +18,7 @@ const APP_SHELL = [
   "./assets/milos-observation-optional.js",
   "./assets/milos-media.js",
   "./assets/milos-pdf.js",
+  "./assets/milos-observation-export.js",
   "./assets/milos-app.js",
   "./assets/milos-evia-v2.js",
   "./course-packs/Bricklayer_ST0095_v1.2.nisi",
