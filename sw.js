89c09c7d4076030f1cdd0ebc347d06b5e4027fa9
DDRNFinch <dfinch1984@hotmail.co.uk>
2026-08-27 17:56:11 +0100

    Cache Milos standardized UI 2.29

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "milos-assessor-shell-v2.28";
+const CACHE_NAME = "milos-assessor-shell-v2.29";
 const CACHE_PREFIX = "milos-assessor-shell-";
 const APP_SHELL = [
   "./",
@@ -36,6 +36,8 @@ const APP_SHELL = [
   "./assets/milos-video-observation-v226.css",
   "./assets/milos-week-calendar-v228.js",
   "./assets/milos-week-calendar-v228.css",
+  "./assets/milos-standard-ui-v229.js",
+  "./assets/milos-standard-ui-v229.css",
   "./assets/milos-review-deadlines-v215.js",
   "./assets/milos-uk-dates-v215.js",
   "./assets/milos-planning-v213.js",
